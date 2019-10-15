a7546c151ab03f1d5e89f9b6343bdf9889bf7686
Alexander A <askord@list.ru>
2019-10-15 22:14:49 +0300

    Проверка наполненности формы, анимация ошибки, фокус, использование хранилища

--- js/script.js
+++ js/script.js
@@ -1,9 +1,53 @@
 var form = document.querySelector ('.hotel-form');
 var button = document.querySelector ('.button-search-hotel');
+var arrival = document.querySelector('[name=arrival-date');
+var departure = form.querySelector('[name=departure-date');
+var adult = form.querySelector('[name=adult-amount');
+var child = form.querySelector('[name=child-amount');
+var isStorageSupport = true;
+var storageadult = "";
+var storagechild = "";
+
+try {
+    storageadult = localStorage.getItem('adult');
+    storagechild = localStorage.getItem('child');
+    } catch (err) {
+    isStorageSupport = false;
+}
 
 form.classList.remove('hotel-form-show');
 
 button.addEventListener('click', function(evt) {
     evt.preventDefault();
     form.classList.toggle('hotel-form-hidden');
-})
+    form.classList.remove('hotel-form-error');
+    if (storageadult && storagechild) {
+        adult.value = storageadult;
+        child.value = storagechild;
+      }
+    arrival.focus();
+});
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (!form.classList.contains('hotel-form-hidden')) {
+        form.classList.add('hotel-form-hidden');
+        form.classList.remove('hotel-form-error');
+      }
+    }
+});
+
+form.addEventListener('submit', function(evt) {
+    if (!arrival.value || !departure.value || !adult.value || !child.value) {
+    evt.preventDefault();
+    form.classList.remove('hotel-form-error');
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("hotel-form-error");
+    } else {
+        if (isStorageSupport) {
+        localStorage.setItem('adult', adult.value);
+        localStorage.setItem('child', child.value);
+    }
+}
+});

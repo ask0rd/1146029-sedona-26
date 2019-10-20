65024d606efa1141bd52618984820cbed631ce93
Alexander A <askord@list.ru>
2019-10-20 14:17:57 +0300

    Правки на вторую защиту
    Добавлен градиент на главную, если не загрузятся картинки; убрана белая полоса в футере; label чекбоксов в состоянии disabled ведет себя так же, как чекбокс; border формы на главной стилизован одинаково в ie ff и chrome; переделано переполнение контентом на главной, в карточке отелей; изображения на главной переименованы согласно контексту; в файле скрипта кавычки приведены к общему виду; постфиксы селекторов на главной сделаны более очевидными и приведены к общему виду; добавлено текстовое описание для ссылок в меню навигации и футере, проверено с помощью Voice Over.

--- js/script.js
+++ js/script.js
@@ -1,56 +1,56 @@
-var form = document.querySelector ('.hotel-form');
-var button = document.querySelector ('.button-search-hotel');
-var arrival = document.querySelector('[name=arrival-date]');
-var departure = form.querySelector('[name=departure-date]');
-var adult = form.querySelector('[name=adult-amount]');
-var child = form.querySelector('[name=child-amount]');
+var form = document.querySelector (".hotel-form");
+var button = document.querySelector (".button-search-hotel");
+var arrival = document.querySelector("[name=arrival-date]");
+var departure = form.querySelector("[name=departure-date]");
+var adult = form.querySelector("[name=adult-amount]");
+var child = form.querySelector("[name=child-amount]");
 var isStorageSupport = true;
 var storageAdult = "";
 var storageChild = "";
 
 try {
-    storageAdult = localStorage.getItem('adult');
-    storageChild = localStorage.getItem('child');
+    storageAdult = localStorage.getItem("adult");
+    storageChild = localStorage.getItem("child");
     } catch (err) {
     isStorageSupport = false;
 }
 
-form.classList.add('hotel-form-hidden');
+form.classList.add("hotel-form-hidden");
 
-button.addEventListener('click', function(evt) {
+button.addEventListener("click", function(evt) {
     evt.preventDefault();
-    form.classList.toggle('hotel-form-hidden');
-    form.classList.remove('hotel-form-error');
+    form.classList.toggle("hotel-form-hidden");
+    form.classList.remove("hotel-form-error");
     if (storageAdult && storageChild) {
         adult.value = storageAdult;
         child.value = storageChild;
       }
-    if (!form.classList.contains('hotel-form-hidden')) {
+    if (!form.classList.contains("hotel-form-hidden")) {
       setTimeout(function() {
         arrival.focus();
     }, 1000); }
 });
 
-window.addEventListener('keydown', function (evt) {
+window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (!form.classList.contains('hotel-form-hidden')) {
-        form.classList.add('hotel-form-hidden');
-        form.classList.remove('hotel-form-error');
+      if (!form.classList.contains("hotel-form-hidden")) {
+        form.classList.add("hotel-form-hidden");
+        form.classList.remove("hotel-form-error");
       }
     }
 });
 
-form.addEventListener('submit', function(evt) {
+form.addEventListener("submit", function(evt) {
     if (!arrival.value || !departure.value || !adult.value || !child.value) {
     evt.preventDefault();
-    form.classList.remove('hotel-form-error');
+    form.classList.remove("hotel-form-error");
     form.offsetWidth = form.offsetWidth;
     form.classList.add("hotel-form-error");
     } else {
         if (isStorageSupport) {
-        localStorage.setItem('adult', adult.value);
-        localStorage.setItem('child', child.value);
+        localStorage.setItem("adult", adult.value);
+        localStorage.setItem("child", child.value);
     }
 }
 });
